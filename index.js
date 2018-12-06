6922ef852d68d371430893254583de1fb84b538e
M.C <hanhchaumn@gmail.com>
2018-12-06 22:04:30 +0700

    Mount static

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const app = new Koa();
 const port = process.env.PORT || 3000;
 const cors = require('@koa/cors');
 const router = require('./routes');
+const mount = require('koa-mount');
 const serve = require('koa-static');
+const path = require('path');
 
 app.use(
 	cors({
 		origin: '*'
 	})
 );
+
 app.use(router.routes());
 
-app.use(serve('./build'));
+const render = serve(path.join(__dirname, '/build'));
+app.use(mount(render));
 
 app.listen(port);
