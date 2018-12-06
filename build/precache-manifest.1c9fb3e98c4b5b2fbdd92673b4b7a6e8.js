0d14d90d54c73fb1e2b73d4450fb196298591573
M.C <hanhchaumn@gmail.com>
2018-12-06 21:31:36 +0700

    Upload built client code

---
+++ build/precache-manifest.1c9fb3e98c4b5b2fbdd92673b4b7a6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56cb8653ce29bdad0979",
+    "url": "/static/js/main.56cb8653.chunk.js"
+  },
+  {
+    "revision": "864cdab1117e3369eeb8",
+    "url": "/static/js/1.864cdab1.chunk.js"
+  },
+  {
+    "revision": "56cb8653ce29bdad0979",
+    "url": "/static/css/main.67cb2f1a.chunk.css"
+  },
+  {
+    "revision": "864cdab1117e3369eeb8",
+    "url": "/static/css/1.9f337777.chunk.css"
+  },
+  {
+    "revision": "0ddd8f4a4d1397e40b5d0298f506663f",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.55fcb05d9476a9c88a2c9b2ae7b78e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3a62d02e051e5c1dfce",
+    "url": "/static/js/main.f3a62d02.chunk.js"
+  },
+  {
+    "revision": "864cdab1117e3369eeb8",
+    "url": "/static/js/1.864cdab1.chunk.js"
+  },
+  {
+    "revision": "f3a62d02e051e5c1dfce",
+    "url": "/static/css/main.67cb2f1a.chunk.css"
+  },
+  {
+    "revision": "864cdab1117e3369eeb8",
+    "url": "/static/css/1.9f337777.chunk.css"
+  },
+  {
+    "revision": "898c08c421ab9435a281cd73e3453601",
+    "url": "/index.html"
+  }
+];
